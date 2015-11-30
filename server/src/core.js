3101dbcb27ca51429938dfb9b353dc96c34f0990
Jiří Žán <jirka@jizan.cz>
2015-11-30 23:30:10 +0100

    Core - setEntries, Next

---
+++ server/src/core.js
@@ -0,0 +1,16 @@
+import {List, Map} from 'immutable';
+
+export function setEntries(state, entries)
+{
+    return state.set('entries', List(entries));
+}
+
+export function next(state)
+{
+    const entries = state.get('entries');
+
+    return state.merge({
+        vote: Map({pair: entries.take(2)}),
+        entries: entries.skip(2)
+    });
+}
