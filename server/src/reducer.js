966c771ce6bcf5d03ea78e38767c9e140c66fd87
Jiří Žán <jirka@jizan.cz>
2015-12-15 23:00:26 +0100

    Reducer

---
+++ server/src/reducer.js
@@ -0,0 +1,17 @@
+import {setEntries, next, vote, INITIAL_STATE} from './core';
+
+export default function reducer(state = INITIAL_STATE, action)
+{
+    switch (action.type) {
+        case 'SET_ENTRIES':
+            return setEntries(state, action.entries);
+            break;
+        case 'NEXT':
+            return next(state);
+            break;
+        case 'VOTE':
+            return vote(state, action.entry);
+            break;
+    }
+    return state;
+}
