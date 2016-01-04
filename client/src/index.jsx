2850b02c461265eec88c85eb110057bd88ec8ceb
Jiří Žán <jirka@jizan.cz>
2016-01-04 20:48:41 +0100

    Introducing client side Redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Results from './components/Results';
 import Voting from './components/Voting';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+});
 
 const routes = <Route component={App}>
     <Route path="/results" component={Results} />
